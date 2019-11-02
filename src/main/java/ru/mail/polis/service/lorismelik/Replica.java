f084a58d594f5366f3a93fdb75c6ab00cf8e5cae
Timur Erniyazov <lorismelik@yandex.ru>
2019-11-02 18:24:09 +0300

    m

--- src/main/java/ru/mail/polis/service/lorismelik/Replica.java
+++ src/main/java/ru/mail/polis/service/lorismelik/Replica.java
@@ -9,23 +9,23 @@ import java.util.List;
 
 import static one.nio.http.Response.BAD_REQUEST;
 
-public class RF {
+public class Replica {
     private final int ack;
     private final int from;
 
-    public RF(final int ack, final int from) {
+    public Replica(final int ack, final int from) {
         this.ack = ack;
         this.from = from;
     }
 
     @NotNull
-    private static RF of(@NotNull final String value) {
+    private static Replica of(@NotNull final String value) {
         final String rem = value.replace("=", "");
         final List<String> values = Splitter.on('/').splitToList(rem);
         if (values.size() != 2) {
             throw new IllegalArgumentException("Wrong replica factor:" + value);
         }
-        return new RF(Integer.parseInt(values.get(0)), Integer.parseInt(values.get(1)));
+        return new Replica(Integer.parseInt(values.get(0)), Integer.parseInt(values.get(1)));
     }
 
     /**
@@ -33,23 +33,23 @@ public class RF {
      *
      * @param replicas    to define the amount of replicas needed
      * @param session     to output responses
-     * @param defaultRF   to specify the default RF
+     * @param defaultReplica   to specify the default RF
      * @param clusterSize to specify the size of cluster
      * @return RF value
      */
-    public static RF calculateRF(final String replicas, @NotNull final HttpSession session,
-                                 final RF defaultRF, final int clusterSize) throws IOException {
-        RF rf = null;
+    public static Replica calculateRF(final String replicas, @NotNull final HttpSession session,
+                                      final Replica defaultReplica, final int clusterSize) throws IOException {
+        Replica replica = null;
         try {
-            rf = replicas == null ? defaultRF : RF.of(replicas);
-            if (rf.ack < 1 || rf.from < rf.ack || rf.from > clusterSize) {
+            replica = replicas == null ? defaultReplica : Replica.of(replicas);
+            if (replica.ack < 1 || replica.from < replica.ack || replica.from > clusterSize) {
                 throw new IllegalArgumentException("From is too big!");
             }
-            return rf;
+            return replica;
         } catch (IllegalArgumentException e) {
             session.sendError(BAD_REQUEST, "Wrong RF!");
         }
-        return rf;
+        return replica;
     }
 
     public int getFrom() {
