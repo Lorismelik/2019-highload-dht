ee8ca24c1096835f8a285ca501051de2a72fa969
Timur Erniyazov <lorismelik@yandex.ru>
2019-11-17 14:24:21 +0300

    fix warning

--- src/main/java/ru/mail/polis/service/lorismelik/Coordinators.java
+++ src/main/java/ru/mail/polis/service/lorismelik/Coordinators.java
@@ -286,7 +286,7 @@ class Coordinators {
         }
     }
 
-    private class ProcessRequestModel {
+    private static class ProcessRequestModel {
         final ByteBuffer key;
         final AtomicInteger recievidAcks = new AtomicInteger(0);
         final Boolean proxied;
